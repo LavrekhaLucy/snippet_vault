25944784825a97d611d61b5343457eac9878c640
LavrekhaLucy <lavrehaludmila@gmail.com>
2026-05-01 10:37:01 +0300

    fix: update port to 8080 and host to 0.0.0.0 for Fly.io deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,8 +13,7 @@ async function bootstrap() {
   }));
 
 
-  // const port = process.env.APP_PORT || 3001;
-  // const host = process.env.APP_HOST ||'localhost';
+  const port = process.env.PORT || 8080;
 
   app.enableCors({
     origin: true,
@@ -22,11 +21,9 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // await app.listen(port, host, () => {
-  //   console.log(`Server is running on port: http://${host}:${port}`);
-  // });
-  await app.listen(3001, '0.0.0.0');
-  console.log(`Application is running on: http://localhost:3001`);
+  await app.listen(port, '0.0.0.0');
+  // console.log(`Application is running on: http://localhost:3001`);
+  console.log(`Application is running on port: ${port}`);
 }
 
 bootstrap();
